849562c157fed5a48a662a9209401a425490a81f
nhthai23-05 <hoangthai.nguyen.0203@gmail.com>
2026-07-04 16:09:34 +0700

    Rerun example + Scribble

--- src/deep_learning/__init__.py
+++ src/deep_learning/__init__.py
@@ -1,10 +1,14 @@
-from .model import Zhang16Net
-from .colorizer import DeepColorizer
-from .dataset import ColorizationDataset, get_dataloaders
 from .utils import compute_metrics, visualize_result, rgb_to_lab, lab_to_rgb, load_config
-from .quantize import ABQuantizer
-from .loss import ClassRebalancedCELoss, HuberColorLoss
-from .train import Trainer
+
+try:
+    from .model import Zhang16Net
+    from .colorizer import DeepColorizer
+    from .dataset import ColorizationDataset, get_dataloaders
+    from .quantize import ABQuantizer
+    from .loss import ClassRebalancedCELoss, HuberColorLoss
+    from .train import Trainer
+except ImportError:
+    pass
 
 __all__ = [
     "Zhang16Net",
